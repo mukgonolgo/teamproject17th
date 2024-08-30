7292d146ce658bb887fe566ce31affacb9e3d95d
GREEN <GREEN@G704-18>
2024-08-30 17:28:13 +0900

    에러페이지 완성

--- src/main/java/com/test/project/MainComtorller.java
+++ src/main/java/com/test/project/MainComtorller.java
@@ -14,21 +14,49 @@ public class MainComtorller {
 	public String login() {
 		return "login";
 	}
+	
+	//리뷰페이지
 	@GetMapping("/review")
 	public String review_page() {
 		return "review_page";
 	}
+	
+	//리뷰 상세페이지
 	@GetMapping("/review_detail")
 	public String review_detail() {
 		return "review_detail";
 	}
-	@GetMapping("/eat")
-	public String eat() {
-		return "eat_friends";	}
+	
+	//리뷰 피드 페이지
 	@GetMapping("/review_feed")
 	public String review_feed() {
 		return "review_feed";
 	}
+	
+	//밥친구 페이지
+	@GetMapping("/eat")
+	public String eat() {
+		return "eat_friends";	
+	}
+	
+	//에러페이지
+	@GetMapping("/error_404")
+	public String error_404() {
+		return "/error_404";	
+	}
+	@GetMapping("/error_500")
+	public String error_500() {
+		return "/error_500";	
+	}
+	@GetMapping("/error_403")
+	public String error_403() {
+		return "/error_403";	
+	}
+	@GetMapping("/error_401")
+	public String error_401() {
+		return "/error_401";	
+	}
+	
 	@GetMapping("/footer")
 	public String bord() {
 		return "footer";
