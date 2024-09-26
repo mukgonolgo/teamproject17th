4e4124cbb3091a691d9a1d2454bc653554450fe9
zimin <tb23ut@brocku.ca>
2024-09-26 14:10:56 +0900

    9/26 front end end end

--- src/main/java/com/test/project/eat_friends/FriendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.project.eat_friends;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendRepository extends JpaRepository<Friend, Long> {
-	 Friend findByFriendTitle(String friendTitle); // 게시글 제목으로 Friend 엔티티 조회
-}
--- src/main/java/com/test/project/review/tag/ReviewTagMapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.project.review.tag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewTagMapRepository extends JpaRepository<ReviewTagMap, Long> {
-    // 추가적인 쿼리 메소드가 필요하다면 여기에 정의
-}
