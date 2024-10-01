dde806e71052285e37c2caa4a5919c2c6fa746c8
Parkjongmin <hubenim@gmail.com>
2024-10-01 10:23:33 +0900

    사진 수정

--- src/main/java/com/test/project/MainController.java
+++ src/main/java/com/test/project/MainController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.test.project.reservation.Reservation;
+import com.test.project.reservation.ReservationService;
 import com.test.project.review.img.ReviewImage;
 import com.test.project.review.img.ReviewImageMap;
 import com.test.project.review.like.ReviewLikeService;
@@ -24,58 +26,54 @@ import com.test.project.user.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-
-
 @RequiredArgsConstructor
 @Controller
 public class MainController {
-	
-		@Autowired
-	    private final ReviewLikeService reviewLikeService;	
-	    private final UserService userService;
-	   
-	    
-	    @Autowired
-		private StoreService storeService;
-	    
+	@Autowired
+	private final ReservationService reservationService;
+
+	@Autowired
+	private final ReviewLikeService reviewLikeService;
+	private final UserService userService;
+
+	@Autowired
+	private StoreService storeService;
+
 	@GetMapping("/")
 	public String root(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        List<Store> storeList = storeService.getAllStore();
-        model.addAttribute("storeList", storeList);
-        
-        
-		if(userDetails != null) {
+		List<Store> storeList = storeService.getAllStore();
+		model.addAttribute("storeList", storeList);
+
+		if (userDetails != null) {
 			SiteUser user = userService.getUser(userDetails.getUsername());
-			model.addAttribute("profileImage",user.getImageUrl());
-			model.addAttribute("username",user.getUsername());
-			model.addAttribute("nickname",user.getNickname());
-			model.addAttribute("userid",user.getId());
+			model.addAttribute("profileImage", user.getImageUrl());
+			model.addAttribute("username", user.getUsername());
+			model.addAttribute("nickname", user.getNickname());
+			model.addAttribute("userid", user.getId());
 
 		}
-		
+
 		return "index";
 	}
-	
+
 	@GetMapping("/mypage/{userid}")
 	public String mypage(@PathVariable("userid") Long userid, Model model) {
-	    Optional<SiteUser> user = userService.getUserById(userid);
-	    
-	    if (user.isPresent()) {
-	        SiteUser siteUser = user.get();
-	        model.addAttribute("user", siteUser);  // 전체 유저 객체 추가
+		Optional<SiteUser> user = userService.getUserById(userid);
 
-	        // 좋아요한 리뷰의 첫 번째 이미지를 가져옴
-	        List<ReviewImageMap> likedImages = reviewLikeService.getFirstImagesForLikedReviews(userid);
-	        model.addAttribute("likedImages", likedImages);  // 좋아요한 이미지 리스트 추가
+		if (user.isPresent()) {
+			SiteUser siteUser = user.get();
+			model.addAttribute("user", siteUser); // 전체 유저 객체 추가
+			List<Reservation> reservations = reservationService.getReservationsByUser(siteUser);
+			model.addAttribute("reservations", reservations);
 
-	        return "user/mypage";  // mypage.html 파일을 렌더링
-	    }
+			// 좋아요한 리뷰의 첫 번째 이미지를 가져옴
+			List<ReviewImageMap> likedImages = reviewLikeService.getFirstImagesForLikedReviews(userid);
+			model.addAttribute("likedImages", likedImages); // 좋아요한 이미지 리스트 추가
 
-	    return "redirect:/error";  // 유저가 없으면 에러 처리
+			return "user/mypage"; // mypage.html 파일을 렌더링
+		}
+
+		return "redirect:/error"; // 유저가 없으면 에러 처리
 	}
 
-
-	
-
 }
-
